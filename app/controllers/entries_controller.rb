40450e075c9e878a7e568af70f8e0e76d8843514
Tyler Lesmann <redhatcat@gmail.com>
2010-10-31 16:01:10 -0600

    Entry previewing!

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,4 +4,14 @@ class EntriesController < ApplicationController
 
   auto_actions :all
 
+  def update
+    if params[:preview]
+      e = Entry.new(params[:entry])
+      e.format_text
+      render :text => e.body
+    else
+      hobo_update
+    end
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Emmett = {
+  preview: function (form) {
+    $('preview').update(
+      '<img src="/images/spinner.gif" alt=""/> Loading preview...');
+    new Ajax.Request('/entries/5-rdoc-test', {
+      parameters: form.serialize() + '&preview=1',
+      onSuccess: function (response) {
+        $('preview').update(response.responseText);
+      }
+    });
+  }
+};
