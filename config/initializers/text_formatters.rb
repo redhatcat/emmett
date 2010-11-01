d904f8b4e5bbb5449da0548ca6056bc742f982a4
Tyler Lesmann <redhatcat@gmail.com>
2010-11-01 10:39:19 -0600

    Tweak for new users

--- config/initializers/text_formatters.rb
+++ config/initializers/text_formatters.rb
@@ -1,17 +1,21 @@
 require 'bbcode_formatter'
 require 'rdoc_formatter'
 
-[
-  ['Plain Text', 'HoboFields::Types::Text'],
-  ['BBCode (BBRuby)', 'BbcodeFormatter'],
-  ['HTML', 'HoboFields::Types::HtmlString'],
-  ['Markdown (Maruku)', 'Maruku'],
-  ['Markdown (BlueCloth)', 'BlueCloth'],
-  ['RDoc', 'RdocFormatter'],
-  ['Textile (RedCloth)', 'RedCloth'],
-].each do |name, class_name|
-  format = TextFormat.find_by_class_name(class_name)
-  if not format
-    TextFormat.create!(:name => name, :class_name => class_name)
+begin
+  [
+    ['Plain Text', 'HoboFields::Types::Text'],
+    ['BBCode (BBRuby)', 'BbcodeFormatter'],
+    ['HTML', 'HoboFields::Types::HtmlString'],
+    ['Markdown (Maruku)', 'Maruku'],
+    ['Markdown (BlueCloth)', 'BlueCloth'],
+    ['RDoc', 'RdocFormatter'],
+    ['Textile (RedCloth)', 'RedCloth'],
+  ].each do |name, class_name|
+    format = TextFormat.find_by_class_name(class_name)
+    if not format
+      TextFormat.create!(:name => name, :class_name => class_name)
+    end
   end
+rescue
+  # text_formats table doesn't exist yet
 end
