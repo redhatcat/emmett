d530e9ffede1ce5b966e0bdffcf75c34e7e1cac8
Tyler Lesmann <redhatcat@gmail.com>
2010-11-01 12:50:03 -0600

    email_from setting

--- app/mailers/emmett_mailer.rb
+++ app/mailers/emmett_mailer.rb
@@ -1,6 +1,8 @@
 class EmmettMailer < ActionMailer::Base
   def new_comment(comment)
     @comment = comment
+    settings = YAML::load(File.open("#{RAILS_ROOT}/config/emmett.yml"))
+    from settings["email_from"]
     recipients comment.entry.user.email_address
     subject "New comment on #{comment.entry.name}"
     content_type "text/html"
